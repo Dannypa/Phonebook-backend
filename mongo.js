a20cb887664aa7c63fce6336fab9c2b34b7d4a46
Daniil Parniukov <dparniukov@gmail.com>
2024-07-17 01:58:04 +0300

    changed so that url is saved as a env variable

--- mongo.js
+++ mongo.js
@@ -1,21 +1,18 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 const assert = require("node:assert")
 
-if (![3, 5].includes(process.argv.length)) { // we need either 3 or 5 args
+if (![2, 4].includes(process.argv.length)) { // we need either 2 or 4 args
     console.log('wrong number of arguments')
     process.exit(1)
 }
 
-const password = process.argv[2]
 
-const url =
-    `mongodb+srv://danny:${password}@fullstackcoursecluster.7vhinzg.mongodb.net/contacts?
-    retryWrites=true&w=majority&appName=FullStackCourseCluster`
-
+const uri = process.env.MONGODB_URI
 
 // mongoose.set('strictQuery', false) // looks like it is not needed currently
 
-mongoose.connect(url)
+mongoose.connect(uri)
 
 const contactSchema = new mongoose.Schema({
     name: String,
@@ -25,15 +22,15 @@ const contactSchema = new mongoose.Schema({
 const Contact = mongoose.model('Contact', contactSchema) // i really want it to be named contacts.
 
 
-if (process.argv.length === 3) {
+if (process.argv.length === 2) {
     Contact.find({}).then(result => {
         console.log("contacts:")
         result.forEach(contact => console.log(`${contact.name} ${contact.number}`))
         mongoose.connection.close()
     })
 } else {
-    console.assert(process.argv.length === 5)
-    const [name, number] = process.argv.slice(3)
+    console.assert(process.argv.length === 4)
+    const [name, number] = process.argv.slice(2)
     const newContact = new Contact({name, number})
     newContact.save().then(_ => {
         console.log(`added ${name} with number ${number} to the phonebook`)
